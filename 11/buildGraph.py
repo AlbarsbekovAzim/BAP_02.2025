ef56267007531131c155e7e2c6c925937668346f
AlbarsvekovAzim <albarsbekovazim@gmail.com>
2025-02-11 22:18:06 +0600

    i have done build graph task

--- 11/buildGraph.py
+++ 11/buildGraph.py
@@ -6,12 +6,29 @@ for i in range(5, 5 + n):
     for j in range(5, 5 + n):
         if not j == i:
             graph[i].add(j)
+def connectconsts(cur):
+    lengths = [len(graph[x]) for x in graph if x <= 4]
+    ind = [index + 1 for index, num in enumerate(lengths) if num == min(lengths) and index + 1 != cur]
+    graph[cur].add(ind[0])
+    graph[ind[0]].add(cur)
 for i in range(1, 5):
     for j in range(2):
-        lengths = [len(graph[x]) for x in graph if x > 4]
-        ind = [index + 5 for index, num in enumerate(lengths) if num == min(lengths)]
-        graph[i].add(ind[0])
-        graph[ind[0]].add(i)
+        if len(graph[i]) < 2:
+            lengths = [len(graph[x]) for x in graph if x > 4]
+            ind = [index + 5 for index, num in enumerate(lengths) if num == min(lengths)]
+            if len(graph) > 4:
+                if not i in graph[ind[0]]:
+                    graph[i].add(ind[0])
+                    graph[ind[0]].add(i)
+                else:
+                    # lengths = [len(graph[x]) for x in graph if x <= 4]
+                    # ind = [index + 1 for index, num in enumerate(lengths) if num == min(lengths)]
+                    # graph[i].add(ind[0])
+                    # graph[ind[0]].add(i)
+                    connectconsts(i)
+            else:
+                connectconsts(i)
+
 
 # print([graph[x] for x in graph])
 
